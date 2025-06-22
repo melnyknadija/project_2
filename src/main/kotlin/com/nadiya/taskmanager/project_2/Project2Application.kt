1035afbfc98fa62a6de327c8b141f46f96df68f1
Nadiya <melnyk.nadiia@chnu.edu.ua>
2025-06-22 18:20:31 +0300

    proj2 lab1

---
+++ src/main/kotlin/com/nadiya/taskmanager/project_2/Project2Application.kt
@@ -0,0 +1,12 @@
+package com.nadiya.taskmanager.project_2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+class Project2Application
+
+fun main(args: Array<String>) {
+    runApplication<Project2Application>(*args)
+}
