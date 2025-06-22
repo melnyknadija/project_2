5907cc954045c7618473b0b55fc224fa7665b5ff
Nadiya <melnyk.nadiia@chnu.edu.ua>
2025-06-22 18:18:46 +0300

    proj2 lab1

--- src/main/kotlin/com/nadiya/taskmanager/project_2/controller/MyController.kt
+++ src/main/kotlin/com/nadiya/taskmanager/project_2/controller/MyController.kt
@@ -1,4 +1,31 @@
 package com.nadiya.taskmanager.project_2.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
+
+@Controller
 class MyController {
+
+    @GetMapping("//")
+    fun index(model: Model): String {
+        model.addAttribute("message", "Привіт з JTE та Spring на Kotlin!")
+        model.addAttribute("userName", "Іван")
+        model.addAttribute("age", 30)
+        model.addAttribute("items", listOf("Яблуко", "Банан", "Апельсин"))
+
+        val userInfo = mutableMapOf<String, String>()
+        userInfo["ім'я"] = "Олена"
+        userInfo["місто"] = "Київ"
+        userInfo["професія"] = "Розробник"
+        model.addAttribute("userInfo", userInfo)
+
+        return "index"
+    }
+
+    @GetMapping("/another")
+    fun anotherPage(): String {
+        return "another_template"
+    }
 }
